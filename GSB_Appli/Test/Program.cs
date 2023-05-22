0e8646cc81835ba2746347ef69a051db9e84d680
qcheymol <80961478+qcheymol@users.noreply.github.com>
2023-05-22 21:49:10 +0200

    Add files via upload

--- GSB_Appli/Test/Program.cs
+++ GSB_Appli/Test/Program.cs
@@ -12,30 +12,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-           /* Medecin unMedecin = DBconnect.GetMedecin(1);
-            Console.WriteLine("medecin = " + unMedecin.ToString());
-            Console.ReadKey();
 
-           List<Medecin> lesMedecins = DBconnect.GetMedecins();
-            foreach (Medecin unmedecin in lesMedecins)
-            {
-                Console.WriteLine("liste = " + unmedecin.ToString());
-            }
-            Console.ReadKey();*/
-
-            /*List<Specialite> lesSpecialite = DBconnect.GetSpecialites();
-            foreach (Specialite uneSpecialite in lesSpecialite)
-            {
-                Console.WriteLine("liste = " + uneSpecialite.Libelle_specialite);
-            }
-            Console.ReadKey();*/
-
-            List<Medicament> LesMedicaments = DBconnect.GetMedicaments();
-            foreach (Medicament uneSpecialite in LesMedicaments)
-            {
-                Console.WriteLine(uneSpecialite.NomCommercial_medicament + " " + uneSpecialite.Famille.Libelle_famille);
-            }
-            Console.ReadKey();
 
         }
     }
